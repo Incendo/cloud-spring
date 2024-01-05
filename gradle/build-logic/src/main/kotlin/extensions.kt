fcf6deb139ac426624316a92f80ceb0cb2a8dc97
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2024-01-04 16:29:15 -0800

    use cloud-build-logic

--- gradle/build-logic/src/main/kotlin/extensions.kt
+++ gradle/build-logic/src/main/kotlin/extensions.kt
@@ -2,5 +2,5 @@ import org.gradle.accessors.dm.LibrariesForLibs
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.the
 
-public val Project.libs: LibrariesForLibs
+val Project.libs: LibrariesForLibs
     get() = the()
