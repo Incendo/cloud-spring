48c24c878addc7c3ae216a6ee7cc8cac9dbc6f8a
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2024-01-29 16:16:41 -0700

    Update build logic and setup javadoc linking

--- cloud-spring/build.gradle.kts
+++ cloud-spring/build.gradle.kts
@@ -1,3 +1,4 @@
+import org.incendo.cloudbuildlogic.JavadocLinksExtension
 import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 plugins {
@@ -22,4 +23,27 @@ dependencies {
     testImplementation(libs.spring.boot.starter.test)
     testImplementation(libs.spring.shell.test)
     testImplementation(libs.spring.shell.test.autoconfiguration)
+}
+
+configurations.javadocLinksJavadoc {
+    // Messed up metadata(?), Gradle won't resolve the Javadocs
+    exclude("io.projectreactor")
+    exclude("org.springframework")
+    exclude("org.springframework.boot")
+    exclude("org.springframework.shell")
+}
+
+javadocLinks {
+    // No Javadocs on central
+    excludes.addAll(
+            "org.hibernate.validator",
+            "org.springframework.boot:spring-boot-starter",
+            "org.springframework.boot:spring-boot-starter-logging",
+            "org.springframework.shell:spring-shell-table",
+            "org.springframework.shell:spring-shell-starter",
+            "org.springframework.shell:spring-shell-standard-commands",
+            "org.apache.tomcat.embed",
+            "org.jline:jline-native",
+            "org.apache.logging.log4j:log4j-to-slf4j",
+    )
 }
